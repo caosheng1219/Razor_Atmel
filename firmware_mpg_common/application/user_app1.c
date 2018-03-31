7e16e13bbe732b2d08658907db28cad6375b984a
caosheng1219 <15195865180@163.com>
2018-03-31 17:19:17 +0800

    1
    1

--- firmware_mpg_common/application/user_app1.c
+++ firmware_mpg_common/application/user_app1.c
@@ -1,6 +1,5 @@
 /**********************************************************************************************************************
 File: user_app1.c                                                                
-Cao sheng
 
 ----------------------------------------------------------------------------------------------------------------------
 To start a new task using this user_app1 as a template:
@@ -88,7 +87,32 @@ Promises:
 */
 void UserApp1Initialize(void)
 {
- 
+  AT91C_BASE_PIOA->PIO_PER    = PIOA_PER_ADD_INIT;
+  AT91C_BASE_PIOA->PIO_PDR    = PIOA_PDR_ADD_INIT;
+  AT91C_BASE_PIOA->PIO_OER    = PIOA_OER_ADD_INIT;
+  AT91C_BASE_PIOA->PIO_ODR    = PIOA_ODR_ADD_INIT;
+  AT91C_BASE_PIOA->PIO_SODR   = PIOA_SODR_ADD_INIT;
+  AT91C_BASE_PIOA->PIO_CODR   = PIOA_CODR_ADD_INIT;
+  
+  AT91C_BASE_PIOB->PIO_PER    = PIOB_PER_ADD_INIT;
+  AT91C_BASE_PIOB->PIO_PDR    = PIOB_PDR_ADD_INIT;
+  AT91C_BASE_PIOB->PIO_OER    = PIOB_OER_ADD_INIT;
+  AT91C_BASE_PIOB->PIO_ODR    = PIOB_ODR_ADD_INIT;
+  AT91C_BASE_PIOB->PIO_SODR   = PIOB_SODR_ADD_INIT;
+  AT91C_BASE_PIOB->PIO_CODR   = PIOB_CODR_ADD_INIT;
+  
+  for(u8 i=0;i<10;i++)
+  {
+  Sound_down();
+  }
+  
+  LedOn(PURPLE);
+  LedOff(GREEN);
+  LedOff(BLUE);
+  
+  LCDCommand(LCD_CLEAR_CMD);
+  LCDMessage(LINE1_START_ADDR,"SIN:MUTE VOL:0");
+  LCDMessage(LINE2_START_ADDR,"ADC12B:0x***"); 
   /* If good initialization, set state to Idle */
   if( 1 )
   {
@@ -97,7 +121,7 @@ void UserApp1Initialize(void)
   else
   {
     /* The task isn't properly initialized, so shut it down and don't run */
-    UserApp1_StateMachine = UserApp1SM_FailedInit;
+    UserApp1_StateMachine = UserApp1SM_Error;
   }
 
 } /* end UserApp1Initialize() */
@@ -128,6 +152,163 @@ void UserApp1RunActiveState(void)
 /* Private functions                                                                                                  */
 /*--------------------------------------------------------------------------------------------------------------------*/
 
+/*----------------------------------------------------------------------------------------------------------------------
+Function: ChangeSign
+
+Description
+When Button3 was pressed,change the sign source
+
+Requires:
+  - u8 u8Mode_
+
+Promises:
+  - Change the sign source and set the I/O
+*/
+void ChangeSign(u8 u8Mode_)
+{
+  AT91C_BASE_PIOB->PIO_SODR   = Re_sign_D;
+  AT91C_BASE_PIOB->PIO_CODR   = Re_sign_C;
+  switch(u8Mode_)
+  {
+  case 0:
+    AT91C_BASE_PIOA->PIO_SODR   = Silence_SODR;
+    AT91C_BASE_PIOA->PIO_CODR   = Silence_CODR;
+    LedOn(PURPLE);
+    LedOff(GREEN);
+    LedOff(BLUE);
+    LedOff(WHITE);
+    LCDMessage(LINE1_START_ADDR,"SIN:MUTE");
+    break;
+    
+  case 1:
+    AT91C_BASE_PIOA->PIO_SODR   = MIC_SODR;
+    AT91C_BASE_PIOA->PIO_CODR   = MIC_CODR;
+    LedOn(BLUE);
+    LedOff(PURPLE);
+    LedOff(GREEN);  
+    LedOff(WHITE);
+    LCDMessage(LINE1_START_ADDR,"SIN:MIC");
+    break; 
+    
+  case 2:
+    AT91C_BASE_PIOA->PIO_SODR   = Phone_SODR;
+    AT91C_BASE_PIOA->PIO_CODR   = Phone_CODR;
+    LedOn(GREEN);
+    LedOff(PURPLE);
+    LedOff(BLUE);
+    LedOff(WHITE);
+    LCDMessage(LINE1_START_ADDR,"SIN:PHO");
+    break;
+
+    
+  default:break;
+  }
+}/* end ChangeSign()*/
+
+/*----------------------------------------------------------------------------------------------------------------------
+Function: Sound_up
+
+Description
+When Buttn0 was pressed,turn up the sound
+
+Requires:
+  - 
+
+Promises:
+  - Give the INC a drop along signal 
+*/
+void Sound_up(void)
+{
+  AT91C_BASE_PIOA->PIO_SODR = Sound_u_SODR;
+  AT91C_BASE_PIOA->PIO_CODR = Sound_u_CODR;
+  
+  for(u8 i=0;i<10;i++)
+  {
+    AT91C_BASE_PIOA->PIO_SODR = INC_L_SODR;
+    AT91C_BASE_PIOA->PIO_CODR = INC_L_CODR;
+    Delay_us(10);
+    AT91C_BASE_PIOA->PIO_SODR = INC_H_SODR;
+    AT91C_BASE_PIOA->PIO_CODR = INC_H_CODR;    
+  }
+  
+}/* end Sound_up()*/
+
+/*----------------------------------------------------------------------------------------------------------------------
+Function: 
+
+Description
+
+
+Requires:
+  - 
+
+Promises:
+  - 
+*/
+void Sound_down(void)
+{
+  AT91C_BASE_PIOA->PIO_SODR = Sound_d_SODR;
+  AT91C_BASE_PIOA->PIO_CODR = Sound_d_CODR;
+  
+  for(u8 i=0;i<10;i++)
+  {
+    AT91C_BASE_PIOA->PIO_SODR = INC_L_SODR;
+    AT91C_BASE_PIOA->PIO_CODR = INC_L_CODR;
+    Delay_us(10);
+    AT91C_BASE_PIOA->PIO_SODR = INC_H_SODR;
+    AT91C_BASE_PIOA->PIO_CODR = INC_H_CODR;    
+  }
+  
+}/* end Sound_down */
+
+/*----------------------------------------------------------------------------------------------------------------------
+Function: 
+
+Description
+
+
+Requires:
+  - 
+
+Promises:
+  - 
+*/
+void Test(void)
+{
+  AT91C_BASE_PIOA->PIO_SODR = Test_v_SODR;
+  AT91C_BASE_PIOA->PIO_CODR = Test_v_CODR;
+  AT91C_BASE_PIOB->PIO_SODR = Re_adc_D;
+  AT91C_BASE_PIOB->PIO_CODR = Re_adc_C;
+  LedOff(BLUE);
+  LedOff(PURPLE);
+  LedOff(GREEN);  
+  LedOn(WHITE);
+  LCDMessage(LINE1_START_ADDR,"SIN:TEST");
+}/* end Test*/
+
+/*----------------------------------------------------------------------------------------------------------------------
+Function: 
+
+Description
+
+
+Requires:
+  - 
+
+Promises:
+  - 
+*/
+void Delay_us(u8 u8time_)
+{
+  u8 i=0;
+  u8 j=0;
+  for(i=0;i<u8time_;i++)
+  {
+    for(j=0;j<48;j++)
+    {
+    }
+  }
+}/* end Delay_us()*/
 
 /**********************************************************************************************************************
 State Machine Function Definitions
@@ -137,27 +318,101 @@ State Machine Function Definitions
 /* Wait for ??? */
 static void UserApp1SM_Idle(void)
 {
+  static u8 u8SignMode = 0;
+  static u16 u16TEST_V=0; 
+  static u8 au8Vol[1]={'0'};
+  static u8 au8ADC[7]={'0','0','0',' ',' ',' ',' '};
+  static bool bTest=FALSE;
+   
+  LedOff(RED);
+  
+  if(WasButtonPressed(BUTTON3))
+  {
+    ButtonAcknowledge(BUTTON3);
+    LedOn(RED);
+    u8SignMode++;
+    
+    if(u8SignMode>2)
+    {
+      u8SignMode=0;
+    }
+    
+    ChangeSign(u8SignMode);
+  }
+  
+  if(WasButtonPressed(BUTTON2))
+  {
+    ButtonAcknowledge(BUTTON2);
+    LedOn(RED);
+    Test();
+    bTest=TRUE;
+    LCDMessage(LINE2_START_ADDR+9,&au8ADC[0]);  
+  }  
+  
+  if(WasButtonPressed(BUTTON0))
+  {
+    ButtonAcknowledge(BUTTON0);
+    LedOn(RED);
+    au8Vol[0]++;
+    
+    if(au8Vol[0]<='9'&&au8Vol[0]>='0')
+    {
+      LCDMessage(LINE1_START_ADDR+13,&au8Vol[0]);
+      LCDClearChars(LINE1_START_ADDR+14,6);
+    }
+    else
+    {
+      au8Vol[0]='9';
+    }
+    
+    Sound_up();
+  }
+  
+  if(WasButtonPressed(BUTTON1))
+  {
+    ButtonAcknowledge(BUTTON1);
+    LedOn(RED);
+    au8Vol[0]--;
+
+    if(au8Vol[0]<='9'&au8Vol[0]>='0')
+    {
+      LCDMessage(LINE1_START_ADDR+13,&au8Vol[0]);
+      LCDClearChars(LINE1_START_ADDR+14,6);
+    }
+    else
+    {
+      au8Vol[0]='0';
+    }    
+    
+    Sound_down();
+  }
+  
+  if(bTest)
+  {
+    if(Adc12StartConversion(ADC12_BLADE_AN0))
+    {
+      u16TEST_V = AT91C_BASE_ADC12B->ADC12B_CDR[ADC12_BLADE_AN0];
+      au8ADC[0]=HexToASCIICharUpper(u16TEST_V/16/16);
+      au8ADC[1]=HexToASCIICharUpper(u16TEST_V/16%16);
+      au8ADC[2]=HexToASCIICharUpper(u16TEST_V%16);
+      bTest=TRUE;
+    }
+  }
 
 } /* end UserApp1SM_Idle() */
     
-#if 0
+
+
 /*-------------------------------------------------------------------------------------------------------------------*/
 /* Handle an error */
 static void UserApp1SM_Error(void)          
 {
   
 } /* end UserApp1SM_Error() */
-#endif
 
-
-/*-------------------------------------------------------------------------------------------------------------------*/
-/* State to sit in if init failed */
-static void UserApp1SM_FailedInit(void)          
-{
-    
-} /* end UserApp1SM_FailedInit() */
 
 
 /*--------------------------------------------------------------------------------------------------------------------*/
 /* End of File                                                                                                        */
 /*--------------------------------------------------------------------------------------------------------------------*/
+
--- firmware_mpg_common/application/user_app1.h
+++ firmware_mpg_common/application/user_app1.h
@@ -23,11 +23,49 @@ Header file for user_app1.c
 /**********************************************************************************************************************
 Type Definitions
 **********************************************************************************************************************/
+/*A16-A   A15-B   A14-CS  A13-C   A12 -INC   A11-UD*/
+#define PIOA_PER_ADD_INIT  (u32)0x8403F807
+#define PIOA_PDR_ADD_INIT  (u32)0x7BFC07F8
+#define PIOA_OER_ADD_INIT  (u32)0xBF55FFF9
+#define PIOA_ODR_ADD_INIT  (u32)0x40AA0006
+#define PIOA_SODR_ADD_INIT (u32)0x8801C000
+#define PIOA_CODR_ADD_INIT (u32)0x30003800
 
+#define PIOB_PER_ADD_INIT  (u32)0x01BFFFB8
+#define PIOB_PDR_ADD_INIT  (u32)0x00400040
+#define PIOB_OER_ADD_INIT  (u32)0x01BFFFF0
+#define PIOB_ODR_ADD_INIT  (u32)0x0040000F
+#define PIOB_SODR_ADD_INIT (u32)0x01BFFE00
+#define PIOB_CODR_ADD_INIT (u32)0x00000010
+
+#define Phone_SODR         (u32)0x00000000
+#define Phone_CODR         (u32)0x0001A000
+#define MIC_SODR           (u32)0x00010000
+#define MIC_CODR           (u32)0x0000A000
+#define Silence_SODR       (u32)0x00008000
+#define Silence_CODR       (u32)0x00002000
+#define Test_v_SODR        (u32)0x0000A000
+#define Test_v_CODR        (u32)0x00010000
+
+#define Sound_u_SODR       (u32)0x00001800
+#define Sound_u_CODR       (u32)0x00004000
+#define Sound_d_SODR       (u32)0x00001000
+#define Sound_d_CODR       (u32)0x00004800
+
+#define INC_H_SODR         (u32)0x00001000
+#define INC_H_CODR         (u32)0x00000000
+#define INC_L_SODR         (u32)0x00000000
+#define INC_L_CODR         (u32)0x00001000
+
+#define Re_sign_D          (u32)0x00000000
+#define Re_sign_C          (u32)0x00000010
+#define Re_adc_D           (u32)0x00000010
+#define Re_adc_C           (u32)0x00000000
 
 /**********************************************************************************************************************
 Constants / Definitions
 **********************************************************************************************************************/
+
 
 
 /**********************************************************************************************************************
@@ -49,15 +87,17 @@ void UserApp1RunActiveState(void);
 /*--------------------------------------------------------------------------------------------------------------------*/
 /* Private functions                                                                                                  */
 /*--------------------------------------------------------------------------------------------------------------------*/
-
-
+void ChangeSign(u8 u8Mode_);
+void Sound_up(void);
+void Sound_down(void);
+void Delay_us(u8 u8time_);
+void Test(void);
 /***********************************************************************************************************************
 State Machine Declarations
 ***********************************************************************************************************************/
 static void UserApp1SM_Idle(void);    
 
 static void UserApp1SM_Error(void);         
-static void UserApp1SM_FailedInit(void);        
 
 
 #endif /* __USER_APP1_H */
@@ -66,3 +106,4 @@ static void UserApp1SM_FailedInit(void);
 /*--------------------------------------------------------------------------------------------------------------------*/
 /* End of File                                                                                                        */
 /*--------------------------------------------------------------------------------------------------------------------*/
+
